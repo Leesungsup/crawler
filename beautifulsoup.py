b75b2700af9b34a11163d860d18a0bfb7473669b
tmdtj <tmdtjq0116@naver.com>
2021-06-15 23:04:13 +0900

    beautifulsoup

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -8,7 +8,24 @@ response = requests.get(url)
 if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup)
+    string=soup.find('a').get_text()
+    print(string)
+    string=soup.find_all('a')
+    print(string)
+    print(string[0])
+    for i in string:
+        print(i['href'])
+    for i in string:
+        print(i.get_text())
+    string=soup.find_all('a',class_="sister")
+    print(string)
+    string=soup.find_all('a',{'class':'sister'})
+    print(string)
+    string=soup.find_all('a',id="link3")
+    print(string)
+    string=soup.find_all('a',{'id':'link3'})
+    print(string)
+
 
 else :
     print(response.status_code)
