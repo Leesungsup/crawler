978804b2ab243c09cf1f922023ac7bd7f40c0b7d
tmdtj <tmdtjq0116@naver.com>
2021-06-15 21:55:12 +0900

    404

--- find.py
+++ find.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x32) AppleWebKit/536.35 (KHTML, like Gecko) Chrome/100.0.0.7 Safari/536.28"}
 url="https://www.transfermarkt.com/"
 #정보요청
-r=requests.get(url,headers=headers)
-#상태확인
-print(r.status_code)
+r=requests.get(url)
+if r.status_code==200:
+    html = response.text
+    responce=BeautifulSoup(html,'html.parser')
+    print(responce.find('p'))
+else:
+    print(r.status_code)
